05a3bb5291e442364acc6d13331ca6937e9be225
George Bissias <bissias@gmail.com>
2019-02-20 16:11:00 -0500

    use distinct data structures for block serde test

--- src/test/graphene_tests.cpp
+++ src/test/graphene_tests.cpp
@@ -274,19 +274,19 @@ BOOST_AUTO_TEST_CASE(compute_optimized_graphene_set_can_serde)
 
 BOOST_AUTO_TEST_CASE(graphene_block_can_serde)
 {
-    CBlock block;
-    CTransaction tx;
-    CDataStream stream(
-        ParseHex("01000000010b26e9b7735eb6aabdf358bab62f9816a21ba9ebdb719d5299e88607d722c190000000008b4830450220070aca4"
-                 "4506c5cef3a16ed519d7c3c39f8aab192c4e1c90d065f37b8a4af6141022100a8e160b856c2d43d27d8fba71e5aef6405b864"
-                 "3ac4cb7cb3c462aced7f14711a0141046d11fee51b0e60666d5049a9101a72741df480b96ee26488a4d3466b95c9a40ac5eee"
-                 "f87e10a5cd336c19a84565f80fa6c547957b7700ff4dfbdefe76036c339ffffffff021bff3d11000000001976a91404943fdd"
-                 "508053c75000106d3bc6e2754dbcff1988ac2f15de00000000001976a914a266436d2965547608b9e15d9032a7b9d64fa4318"
-                 "8ac00000000"),
-        SER_DISK, CLIENT_VERSION);
 
     // regular graphene block
     {
+        CBlock block;
+        CTransaction tx;
+        CDataStream stream(
+            ParseHex("01000000010b26e9b7735eb6aabdf358bab62f9816a21ba9ebdb719d5299e88607d722c190000000008b4830450220070aca4"
+                     "4506c5cef3a16ed519d7c3c39f8aab192c4e1c90d065f37b8a4af6141022100a8e160b856c2d43d27d8fba71e5aef6405b864"
+                     "3ac4cb7cb3c462aced7f14711a0141046d11fee51b0e60666d5049a9101a72741df480b96ee26488a4d3466b95c9a40ac5eee"
+                     "f87e10a5cd336c19a84565f80fa6c547957b7700ff4dfbdefe76036c339ffffffff021bff3d11000000001976a91404943fdd"
+                     "508053c75000106d3bc6e2754dbcff1988ac2f15de00000000001976a914a266436d2965547608b9e15d9032a7b9d64fa4318"
+                     "8ac00000000"),
+            SER_DISK, CLIENT_VERSION);
         stream >> tx;
         const CTransactionRef ptx = MakeTransactionRef(tx);
         block.vtx.push_back(ptx);
@@ -300,6 +300,16 @@ BOOST_AUTO_TEST_CASE(graphene_block_can_serde)
     
     // compute optimized graphene block
     {
+        CBlock block;
+        CTransaction tx;
+        CDataStream stream(
+            ParseHex("01000000010b26e9b7735eb6aabdf358bab62f9816a21ba9ebdb719d5299e88607d722c190000000008b4830450220070aca4"
+                     "4506c5cef3a16ed519d7c3c39f8aab192c4e1c90d065f37b8a4af6141022100a8e160b856c2d43d27d8fba71e5aef6405b864"
+                     "3ac4cb7cb3c462aced7f14711a0141046d11fee51b0e60666d5049a9101a72741df480b96ee26488a4d3466b95c9a40ac5eee"
+                     "f87e10a5cd336c19a84565f80fa6c547957b7700ff4dfbdefe76036c339ffffffff021bff3d11000000001976a91404943fdd"
+                     "508053c75000106d3bc6e2754dbcff1988ac2f15de00000000001976a914a266436d2965547608b9e15d9032a7b9d64fa4318"
+                     "8ac00000000"),
+            SER_DISK, CLIENT_VERSION);
         stream >> tx;
         const CTransactionRef ptx = MakeTransactionRef(tx);
         block.vtx.push_back(ptx);
