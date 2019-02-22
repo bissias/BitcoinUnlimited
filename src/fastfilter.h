bb89b2e0266cda76c9693bc0bb35ea125b8af5c7
George Bissias <bissias@gmail.com>
2019-02-22 10:01:16 -0500

    CFastFilter no longer inherits from CVariableFastFilter

--- src/fastfilter.h
+++ src/fastfilter.h
@@ -14,33 +14,11 @@
 const uint32_t MAX_32_BIT = (int)pow(2, 32) - 1;
 class uint256;
 
-// Statically evaluated expression to return whether a number is a power of 2
-constexpr bool isPow2(unsigned int num) { return num && !(num & (num - 1)); }
 /**
- * FastFilter is a probabilistic filter.  The filter can answer whether an element
- * definitely is NOT in the set, but only that an element is LIKELY in the set.
- * This is similar to a Bloom filter, but much faster.
- *
- * This filter expects that the input elements have a random distribution (i.e. hashes), and so does not hash the
- * input again.  This is how it gains the majority of its performance improvement.
- *
  * This class can be used anywhere a Bloom filter is used so long as the input data is random.
  *
  * If nHashFuncs is 16 and nFilterItems is >= 64k all bits in the uint256 input data will be used to set bits in
- * the filter.  If these fields are set to lower numbers, fewer bits may be used (although in the nHashFuncs case
- * execution will be faster).  Therefore, if this structure is used in an application that accepts externally created
- * uint256 numbers that are sensitive to deliberately constructed collisions, be sure to keep nHashFuncs high enough
- * that the creation of collisions in the used bits is not feasible.
- *
- * Note also that the input bits are used without obfuscation or mixing so if an attacker can control some input bits
- * the attacker can cause collisions in some of the fast filter entries for his inputs.  This will cause higher false
- * positive rates for these transactions.  For example, if the attacker can control 32 bits of the input, he can
- * effectively reduce the number of hash functions in the fast filter by 2 because he has engineered a guaranteed
- * collision for the two functions that use those bits.
- *
- * This class is thread-safe in the sense that simultaneous calls to member functions will not crash,
- * but "inserts" may be lost.  However, if you are using this class as an in-ram filter before doing a more expensive
- * operation, a lost insert may be acceptable.
+ * the filter.   
  *
  * nHashFuncs may range from 2 to 32 inclusive.
  */
@@ -124,10 +102,44 @@ public:
     }
 };
 
-
+// Statically evaluated expression to return whether a number is a power of 2
+constexpr bool isPow2(unsigned int num) { return num && !(num & (num - 1)); }
+/**
+ * FastFilter is a probabilistic filter.  The filter can answer whether an element
+ * definitely is NOT in the set, but only that an element is LIKELY in the set.
+ * This is similar to a Bloom filter, but much faster.
+ *
+ * This filter expects that the input elements have a random distribution (i.e. hashes), and so does not hash the
+ * input again.  This is how it gains the majority of its performance improvement.
+ *
+ * This class can be used anywhere a Bloom filter is used so long as the input data is random.
+ *
+ * If NUM_HASH_FNS is 16 and FILTER_SIZE is >= 64k all bits in the uint256 input data will be used to set bits in
+ * the filter.  If these fields are set to lower numbers, fewer bits may be used (although in the NUM_HASH_FNS case
+ * execution will be faster).  Therefore, if this structure is used in an application that accepts externally created
+ * uint256 numbers that are sensitive to deliberately constructed collisions, be sure to keep NUM_HASH_FNS high enough
+ * that the creation of collisions in the used bits is not feasible.
+ *
+ * Note also that the input bits are used without obfuscation or mixing so if an attacker can control some input bits
+ * the attacker can cause collisions in some of the fast filter entries for his inputs.  This will cause higher false
+ * positive rates for these transactions.  For example, if the attacker can control 32 bits of the input, he can
+ * effectively reduce the number of hash functions in the fast filter by 2 because he has engineered a guaranteed
+ * collision for the two functions that use those bits.
+ *
+ * This class is thread-safe in the sense that simultaneous calls to member functions will not crash,
+ * but "inserts" may be lost.  However, if you are using this class as an in-ram filter before doing a more expensive
+ * operation, a lost insert may be acceptable.
+ *
+ * FILTER_SIZE must be a power of 2, and NUM_HASH_FNS may range from 2 to 16 inclusive.  Since hashes are calculated
+ * in pairs of 2, odd values of NUM_HASH_FNS are rounded down.
+ */
 template <unsigned int FILTER_SIZE, unsigned int NUM_HASH_FNS = 16>
-class CFastFilter : public CVariableFastFilter
+class CFastFilter
 {
+protected:
+    // A bit vector containing the bloom filter data
+    std::vector<unsigned char> vData;
+
 public:
     enum
     {
@@ -138,12 +150,70 @@ public:
     {
         static_assert((NUM_HASH_FNS > 1) && (NUM_HASH_FNS <= 16), "NUM_HASH_FNS must be between 2 and 16 inclusive");
         static_assert(isPow2(FILTER_SIZE) && (FILTER_SIZE > 1), "FILTER_SIZE must be a power of 2 greater than 1");
-        nHashFuncs = NUM_HASH_FNS;
-        nFilterItems = FILTER_SIZE;
-
         FastRandomContext insecure_rand;
         vData.resize(FILTER_BYTES);
     }
+
+
+    // returns true IF this function made a change (i.e. the value was previously not set).
+    bool checkAndSet(const uint256 &hash)
+    {
+        const uint32_t *pos = (const uint32_t *)hash.begin();
+        bool unset = 0; // If any position is not set, then this will be true
+        for (unsigned int i = 0; i < NUM_HASH_FNS / 2; i++, pos++)
+        {
+            uint32_t val = *pos;
+            uint32_t idx = val & (FILTER_SIZE - 1);
+            uint32_t bit = (1 << (idx & 7));
+            idx >>= 3;
+            unset |= (0 == (vData[idx] & bit));
+
+            val = __builtin_bswap32(val);
+            uint32_t idx2 = val & (FILTER_SIZE - 1);
+            uint32_t bit2 = (1 << (idx2 & 7));
+            idx2 >>= 3;
+
+            unset |= (0 == (vData[idx2] & bit2));
+
+            vData[idx] |= bit;
+            vData[idx2] |= bit2;
+        }
+        return unset;
+    }
+
+    void insert(const uint256 &hash)
+    {
+        const uint32_t *pos = (const uint32_t *)hash.begin();
+        for (unsigned int i = 0; i < NUM_HASH_FNS / 2; i++, pos++)
+        {
+            uint32_t val = *pos;
+            uint32_t idx = val & (FILTER_SIZE - 1);
+            val = __builtin_bswap32(val);
+            uint32_t idx2 = val & (FILTER_SIZE - 1);
+
+            vData[idx >> 3] |= (1 << (idx & 7));
+            vData[idx2 >> 3] |= (1 << (idx2 & 7));
+        }
+    }
+
+    bool contains(const uint256 &hash) const
+    {
+        const uint32_t *pos = (const uint32_t *)hash.begin();
+        bool unset = 0; // If any position is not set, then this will be true
+        for (unsigned int i = 0; i < NUM_HASH_FNS / 2; i++, pos++)
+        {
+            uint32_t val = *pos;
+            uint32_t idx = val & (FILTER_SIZE - 1);
+            val = __builtin_bswap32(val);
+            uint32_t idx2 = val & (FILTER_SIZE - 1);
+
+            unset |= (0 == (vData[idx >> 3] & (1 << (idx & 7))));
+            unset |= (0 == (vData[idx2 >> 3] & (1 << (idx2 & 7))));
+        }
+        return !unset;
+    }
+
+    void reset() { memset(&vData[0], 0, FILTER_BYTES); }
 };
 
 
